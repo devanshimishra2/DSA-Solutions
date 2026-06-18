e42e76ac5e5b257c0e061247020453c0e8647e36
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-06-19 02:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (95.64%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double x = hour + minutes / 60.0;
+        double diff = fmod(11.0 * x, 12.0);
+        return min(diff, 12.0 - diff) * 30.0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
