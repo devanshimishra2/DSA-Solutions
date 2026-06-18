4f3f765a4ef946ddb39646eed5389b16324cda75
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-06-18 15:35:14 +0530

    Time: 455 ms (21.15%), Space: 225.5 MB (8.8%) - LeetHub - Time: O(n*2*3), Space: O(n*2*3)

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
+int solve(int i , int buy, int cap, vector<int>& prices, vector<vector<vector<int>>>& dp){
+    if(i == prices.size() || cap == 0){
+        return 0;
+    }
+    if(dp[i][buy][cap] != -1){
+        return dp[i][buy][cap];
+    }
+    int profit;
+    if(buy){
+        profit = max(-prices[i]+solve(i+1, 0, cap, prices, dp) , solve(i+1,1, cap, prices, dp));
+    }
+    else{
+        profit = max(prices[i] + solve(i+1, 1, cap - 1, prices, dp), solve(i+1, 0, cap, prices, dp));
+    }
+    return dp[i][buy][cap] = profit;
+}
     int maxProfit(vector<int>& prices) {
-        int firstbuy = INT_MIN;
-        int firstsell = 0;
-        int secondbuy = INT_MIN;
-        int secondsell = 0;
-        for(int price : prices){
-            firstbuy = max(firstbuy , -price);
-            firstsell = max(firstsell, firstbuy + price);
-            secondbuy = max(secondbuy, firstsell - price);
-            secondsell = max(secondsell, secondbuy + price);
-        }
-        return secondsell;
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3,-1)));
+        return solve(0,1,2, prices, dp);
     }
 };
 
