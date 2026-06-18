df66e38dfd6ceb3530d79ebc9cc2986dfa72c640
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-06-18 15:35:18 +0530

    Time: 455 ms (21.15%) | Memory: 225.5 MB (8.80%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int solve(int i , int buy, int cap, vector<int>& prices, vector<vector<vector<int>>>& dp){
+    if(i == prices.size() || cap == 0){
+        return 0;
+    }
+    if(dp[i][buy][cap] != -1){
+        return dp[i][buy][cap];
+    }
+    int profit;
+    if(buy){
+        profit = max(-prices[i]+solve(i+1, 0, cap, prices, dp) , solve(i+1,1, cap, prices, dp));
+    }
+    else{
+        profit = max(prices[i] + solve(i+1, 1, cap - 1, prices, dp), solve(i+1, 0, cap, prices, dp));
+    }
+    return dp[i][buy][cap] = profit;
+}
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3,-1)));
+        return solve(0,1,2, prices, dp);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
