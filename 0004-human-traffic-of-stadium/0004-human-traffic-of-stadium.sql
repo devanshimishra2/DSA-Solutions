e39df6cdb06ccdca1b45b10e3746c6bcc5201b1a
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-06-20 03:13:51 +0530

    Time: 370 ms (79.65%), Space: 0B (100%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0004-human-traffic-of-stadium/0004-human-traffic-of-stadium.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+SELECT id, visit_date, people  FROM (
+    SELECT *, 
+          id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100      
+) t
+WHERE grp IN(
+     SELECT grp
+     FROM (
+        SELECT id- ROW_NUMBER() OVER (ORDER BY id) AS grp
+        FROM Stadium
+        WHERE people >= 100
+     )x
+     GROUP BY grp
+     HAVING COUNT(*) >= 3
+)
+ORDER BY visit_date;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
