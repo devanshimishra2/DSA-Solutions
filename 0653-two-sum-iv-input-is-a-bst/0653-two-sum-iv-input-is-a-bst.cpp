ae8e53f52222b61d1e06961afb9011cc63aee4b5
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-06-20 02:31:59 +0530

    Time: 11 ms (17.34%), Space: 39 MB (15.03%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_set<int>set;
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(root == NULL){
+            return false;
+        }
+        if(set.count(k - root->val)) return true;
+        set.insert(root->val);
+        return findTarget(root->left, k) || findTarget(root->right, k);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
