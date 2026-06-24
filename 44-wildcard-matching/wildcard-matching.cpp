da25101c76a866cf736d6811793e27db9316f894
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-06-25 00:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (93.89%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int si = 0, pi = 0, match = 0, star = -1;
+        int sn = s.length(), pn = p.length();
+        while (si < sn) {
+            if (pi < pn && (p[pi] == '?' || p[pi] == s[si])) {
+                si++;
+                pi++;
+            } else if (pi < pn && p[pi] == '*') {
+                star = pi;
+                match = si;
+                pi++;
+            } else if (star != -1) {
+                pi = star + 1;
+                match++;
+                si = match;
+            } else {
+                return false;
+            }
+        }
+        while (pi < pn && p[pi] == '*') {
+            pi++;
+        }
+        return pi == pn;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
