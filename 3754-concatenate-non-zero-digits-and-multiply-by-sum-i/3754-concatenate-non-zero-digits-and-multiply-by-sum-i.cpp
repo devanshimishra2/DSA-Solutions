0e2d6afca6d820eeb03eb6e70d2d57040eb3ce55
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-07-07 23:46:12 +0530

    Time: 0 ms (100%), Space: 9 MB (51.79%) - LeetHub - Time: O(, Space: O(?)

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long sum = 0, x = 0;
+        for(char c : to_string(n)){
+            if(c !=  '0'){
+                x = x * 10 + c - '0';
+                sum += c - '0';
+            }
+        }
+        return x * sum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
