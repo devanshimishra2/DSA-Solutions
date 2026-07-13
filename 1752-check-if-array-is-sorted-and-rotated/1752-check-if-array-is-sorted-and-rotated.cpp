c1d89b90612c324cdfa2a692a2516b6386b9ab53
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-07-13 23:27:55 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.34%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for(int i = 0; i < n ; i++){
+            if(nums[i]> nums[(i+1) % n ]){
+                count++;
+            }
+        }
+        return count <= 1;
+
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
