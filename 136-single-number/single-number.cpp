af56721fb44ffbefee7955e1197002e3e87635f9
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-07-27 16:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (59.21%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int XOR =0;
-        for(int i = 0; i < nums.size(); i++){
-            XOR ^= nums[i];
+        int n = nums.size();
+        int XOR = 0;
+        for(int i = 0; i < n; i++){
+            XOR = XOR^ nums[i];
         }
         return XOR;
     }
