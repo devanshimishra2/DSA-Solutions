d0fdd772bbe1740f8e52c35b5a443ea986b8b93e
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-07-29 02:15:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.19%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long long power = n;
+        if(power < 0){
+            x = 1/x;
+            power = -power;
+        }
+        while(power > 0){
+            if(power % 2 == 1){
+                ans *= x;
+
+            }
+            x *= x;
+            power /= 2;
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
