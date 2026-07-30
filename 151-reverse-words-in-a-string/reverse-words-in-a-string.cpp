6a9831578784f3ec392e2dfbbabd3af50dc649cd
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-07-30 20:11:15 +0530

    Time: 2 ms (40.36%) | Memory: 10.1 MB (79.65%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> words;
-        stringstream ss(s);
-        string temp;
-        while(ss >> temp){
-            words.push_back(temp);;
+        string ans = "";
+        int i = s.size() - 1;
+        while(i >= 0) {
+            while(i >= 0 && s[i] == ' ')
+                i--;
+            if(i < 0)
+                break;
+            int j = i;
+            while(j >= 0 && s[j] != ' ')
+                j--;
+            ans += s.substr(j + 1, i - j);
+            ans += " ";
+            i = j;
         }
-        string ans;
-        for(int i = words.size() - 1; i >= 0; --i){
-            if(i != words.size() - 1) ans+= " ";
-            ans += words[i];
-        }
+        ans.pop_back();
         return ans;
     }
 };
