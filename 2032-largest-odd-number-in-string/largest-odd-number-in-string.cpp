2614d225254ce9faef018499cf9921f8b4a8c294
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-08-05 19:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.51%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for(int i = num.size(); i >= 0; i--){
+            if((num[i] - '0')% 2 == 1){
+                return num.substr(0, i + 1);
+            }
+
+        }
+        return "";
+    }
+};   
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
