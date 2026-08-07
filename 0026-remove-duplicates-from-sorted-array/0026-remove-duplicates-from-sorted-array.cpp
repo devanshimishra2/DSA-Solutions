f4cc09c1d11109af6291f5ac115fe0a36febc9d6
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-08-07 22:04:21 +0530

    Time: 0 ms (100%), Space: 22.5 MB (81.33%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int index = 1;
+        for(int i = 1; i < n; i++){
+            if(nums[i] != nums[i-1]){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
