3efc7a5a94a5fdf126b90ef8622830067b129997
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-08-10 22:26:11 +0530

    Time: 5 ms (83.18%) | Memory: 11.3 MB (86.66%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr) return nullptr;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow == fast){ 
+                ListNode* entry = head;
+                while(entry != slow){
+                    entry = entry->next;
+                    slow = slow->next;
+                }
+                return entry; 
+            }
+        }
+        return nullptr;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
