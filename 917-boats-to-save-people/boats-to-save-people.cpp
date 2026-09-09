a62fee8619ff6b6f5a4a815e4ed15e421d90958d
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-09-10 00:23:10 +0530

    Time: 27 ms (5.88%) | Memory: 45.8 MB (71.51%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int left = 0;
+        int right = people.size() - 1;
+        int boats = 0;
+        while(left<= right){
+            if(people[left]+ people[right] <= limit){
+                left++;
+            }
+            right--;
+            boats++;
+        }
+        return boats;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
