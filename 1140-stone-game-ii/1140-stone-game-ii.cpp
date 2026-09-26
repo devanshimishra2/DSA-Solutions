1286c470a37cc1d9f13217aca7b66507b484654f
Devanshi Mishra <aids23007@glbitm.ac.in>
2026-09-27 01:24:28 +0530

    Time: 11 ms (48.13%), Space: 13.7 MB (53.29%) - LeetHub - Time: O(n^3), Space: O(n^2)

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n+1, 0));
+        vector<int> suffixsum(n, 0);
+        suffixsum[n-1] = piles[n-1];
+        for(int i = n - 2; i >= 0; i--){
+            suffixsum[i] = suffixsum[i+1] + piles[i];
+        }
+        for(int i = n -1; i >= 0; i--){
+            for(int m = 1; m <= n; m++){
+                if(i+2 * m >= n){
+                    dp[i][m] = suffixsum[i];
+                }
+                else{
+                    for(int  x = 1; x <= 2*m; x++){
+                        dp[i][m] = max(dp[i][m], suffixsum[i] - dp[i + x][max(m, x)]);
+                    }
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
